c2fa04903ad6bb7d3a6bd0a9148dcde99ec2b601
Frank <lifrank@grinnell.edu>
2020-09-22 00:22:35 -0500

    write tests

---
+++ tests/ui/disallowed_method.rs
@@ -0,0 +1,37 @@
+#![warn(clippy::disallowed_method)]
+
+struct Foo;
+
+impl Foo {
+    fn bad_method() {}
+}
+
+struct ImplStruct;
+
+trait Baz{
+    fn bad_method() {}
+}
+
+struct NormalStruct;
+
+impl NormalStruct {
+    fn bad_method() {}
+}
+
+struct AttrStruct {
+    bad_method: i32,
+}
+
+fn main() {
+    let f = Foo;
+    let b = ImplStruct;
+    let n = NormalStruct;
+    let a = AttrStruct{ bad_method: 5 };
+
+    // lint these
+    f.bad_method();
+    b.bad_method();
+    // these are good
+    n.bad_method();
+    a.bad_method;
+}
